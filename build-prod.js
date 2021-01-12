dfd91b0ace8e83700e49d708a273e21dc9669a2b
estebanrfp <estebanrfp@gmail.com>
2021-01-12 11:56:10 +0100

    add build-prod esbuild production file

--- build-prod.js
+++ build-prod.js
@@ -1,16 +1,42 @@
-require('esbuild').build({
-  entryPoints: ['./lib/index.ts'],
-  bundle: true,
-  minify: true,
-  outdir: 'dist',
-  loader: {
-    '.html': 'text'
-  },
-  define: {
-    'process.env.NODE_ENV': '"production"'
-  },
-  target: ['safari14'],
-  format: 'esm',
-  sourcemap: true,
-  splitting: true
-}).catch(() => process.exit(1))
+const workboxBuild = require('workbox-build')
+
+require('esbuild')
+  .build({
+    entryPoints: ['./lib/index.ts'],
+    bundle: true,
+    minify: true,
+    outdir: 'dist',
+    loader: {
+      '.html': 'text'
+    },
+    define: {
+      'process.env.NODE_ENV': '"production"'
+    },
+    target: [
+      'es2020',
+      'chrome58',
+      'firefox57',
+      'safari11',
+      'edge16',
+      'node12.19.0'
+    ],
+    format: 'esm',
+    sourcemap: true,
+    splitting: true
+  })
+  .then(() => {
+    workboxBuild
+      .generateSW({
+        swDest: './dist/sw.js',
+        globDirectory: './dist',
+        skipWaiting: true
+      })
+      .then(() => {
+        console.log('Generated new service worker.')
+      })
+      .catch((err) => {
+        console.error('Unable to generate a new service worker.', err)
+      })
+  })
+
+  .catch(() => process.exit(1))
