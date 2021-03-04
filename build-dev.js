ac17f8322edf66cb0e59712d8d9dcd5a3e42233b
estebanrfp <estebanrfp@gmail.com>
2021-03-04 23:34:35 +0100

    update land

--- build-dev.js
+++ build-dev.js
@@ -1,11 +1,14 @@
 
+const esbuild = require('esbuild')
 const chokidar = require('chokidar')
+const autoprefixer = require('autoprefixer')
+const postCssPlugin = require('@deanc/esbuild-plugin-postcss')
 
-chokidar.watch('.', { ignored: /dev|node_modules/ }).on('all', (event, path) => {
+chokidar.watch('.', { ignored: /dev|.git|node_modules/ }).on('all', (event, path) => {
   console.log(event, path)
 
-  require('esbuild').build({
-    entryPoints: ['./lib/index.ts'],
+  esbuild.build({
+    entryPoints: ['./lib/index.ts', './lib/index.css'],
     bundle: true,
     minify: false,
     outdir: 'dev',
@@ -13,11 +16,35 @@ chokidar.watch('.', { ignored: /dev|node_modules/ }).on('all', (event, path) => 
       '.html': 'text'
     },
     define: {
-      'process.env.NODE_ENV': '"production"'
+      'process.env.NODE_ENV': '"development"'
     },
-    // target: ['safari14'],
+    target: [
+      'es2020',
+      'chrome58',
+      'firefox57',
+      'safari11',
+      'edge16',
+      'node12.19.0'
+    ],
     format: 'esm',
     sourcemap: true,
-    splitting: true
+    splitting: true,
+    plugins: [
+      postCssPlugin({
+        plugins: [autoprefixer]
+      })
+    ]
   }).catch(() => process.exit(1))
 })
+
+// bs.watch('src/**/*.js', function (event, file) {
+//   require('esbuild').build({
+//     stdio: 'inherit',
+//     entryPoints: ['./src/scripts/index.js'],
+//     outfile: `${dist}/assets/scripts${!(process.env.NODE_ENV === 'development') ? '.min' : ''}.js`,
+//     minify: !(process.env.NODE_ENV === 'development'),
+//     bundle: true,
+//     sourcemap: process.env.NODE_ENV === 'development'
+//   }).then(() => bs.reload())
+//     .catch(() => process.exit(1))
+// })
